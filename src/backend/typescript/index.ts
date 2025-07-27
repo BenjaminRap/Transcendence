c5689d2406e5582149699da18b6166ced9d285ec
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-07-27 09:16:12 +0200

    Added fastify-sqlite-typed into the backend

--- src/backend/typescript/index.ts
+++ src/backend/typescript/index.ts
@@ -1,16 +1,42 @@
 import Fastify from 'fastify'
+import { fpSqlitePlugin } from 'fastify-sqlite-typed';
 
 const fastify = Fastify({
-  logger: true
+	logger: true
 })
 
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+fastify.register(fpSqlitePlugin, {
+	dbFilename: "../database.db",
+});
+
+fastify.get('/', (_request, reply) => {
+	reply.send({ hello: 'world' });
 })
 
-fastify.listen({ port: 8181, host: '0.0.0.0' }, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-})
+async function	init() : Promise<void>
+{
+	await fastify.ready();
+	await fastify.db.all(`
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			email TEXT UNIQUE NOT NULL
+		);
+	`);
+}
+
+async function	start() : Promise<void>
+{
+	try
+	{
+		await init();
+		await fastify.listen({ port: 8181, host: '0.0.0.0' });
+		fastify.log.info(`Server listening at address 0.0.0.0:8181`)
+	}
+	catch (error)
+	{
+		fastify.log.error(`Could not launch the server, error : ${error}`);	
+	}
+}
+
+start();
