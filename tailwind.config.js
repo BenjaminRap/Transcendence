2decf1d6cd32efee6cf043cad1aa76febf799589
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-08-07 13:25:50 +0200

    Fixed the indentation of the .config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-/** tailwind.config.js */
 module.exports = {
-  content: [
-    "./src/frontend/**/*.html",
-    "./src/frontend/**/*.ts",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./src/frontend/**/*.html",
+		"./src/frontend/**/*.ts",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: './src/frontend/dev/',
-  build: {
-    outDir: '../../../dockerFiles/nginx/website/',  // IMPORTANT : chemin relatif à root
-    emptyOutDir: true   // vide le dossier prod avant chaque build
-  },
-  publicDir: 'public'   // ce chemin est relatif à root (donc dev/public)
+	root: './src/frontend/dev/',
+	build: {
+		outDir: '../../../dockerFiles/nginx/website/',  // IMPORTANT : chemin relatif à root
+		emptyOutDir: true   // vide le dossier prod avant chaque build
+	},
+	publicDir: 'public'   // ce chemin est relatif à root (donc dev/public)
 })
