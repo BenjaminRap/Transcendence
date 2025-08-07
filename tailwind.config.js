6a4cf914b41675990b22d54e20e34b710e219e78
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-08-07 13:18:54 +0200

    Added the config js files that were ignored by the gitignore, changed the gitignore to only ignore js in the backend

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** tailwind.config.js */
+module.exports = {
+  content: [
+    "./src/frontend/**/*.html",
+    "./src/frontend/**/*.ts",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
