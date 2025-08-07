47ee636d7585eeaab00da05682109a244e7ef204
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-08-07 19:14:53 +0200

    Setup a the basic babylonjs example of the babylonjs website

--- src/frontend/dev/src/typescript/index.ts
+++ src/frontend/dev/src/typescript/index.ts
@@ -1,1 +1,29 @@
-console.log("blibloup");
+import "@babylonjs/loaders/glTF";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, MeshBuilder } from "@babylonjs/core"
+
+class App {
+    constructor() {
+        // create the canvas html element and attach it to the webpage
+        var canvas = document.createElement("canvas");
+        canvas.style.width = "100%";
+        canvas.style.height = "100%";
+        canvas.id = "gameCanvas";
+        document.body.appendChild(canvas);
+
+        // initialize babylon scene and engine
+        var engine = new Engine(canvas, true);
+        var scene = new Scene(engine);
+
+        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        camera.attachControl(canvas, true);
+        new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+
+        // run the main render loop
+        engine.runRenderLoop(() => {
+            scene.render();
+        });
+    }
+}
+
+new App();
