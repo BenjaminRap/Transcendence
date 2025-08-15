f058d7a6ea104fd84d2cc25eaa80160b82d80204
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-08-15 10:23:26 +0200

    The RotatingCube now also moves up and down

--- src/frontend/dev/src/typescript/attachedScripts/RotatingCube.ts
+++ src/frontend/dev/src/typescript/attachedScripts/RotatingCube.ts
@@ -1,18 +1,31 @@
 import { Scene } from "@babylonjs/core/scene";
 import { TransformNode } from "@babylonjs/core/Meshes";
 import { SceneManager, ScriptComponent } from "@babylonjs-toolkit/next";
+import { Tools } from "@babylonjs/core/Misc/tools";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 
 export class RotatingCube extends ScriptComponent {
 	public	rotationSpeed : number = 1.0;
+	private _initialPosition : Vector3;
 
     constructor(transform: TransformNode, scene: Scene, properties: any = {}, alias: string = "RotatingCube") {
         super(transform, scene, properties, alias);
+		this._initialPosition = transform.position;
     }
 
     protected update(): void {
-        /* Update render loop function */
 		this.transform.addRotation(0, this.rotationSpeed * this.getDeltaSeconds(), 0);
+		this.moveUpAndDown();
     }
+
+	private moveUpAndDown() : void {
+		const	maximunOffsetLength : number = 1;
+		const	speed = 0.002;
+		const	yOffset : number = Math.sin(Tools.Now * speed) * maximunOffsetLength;
+		const	offset = new Vector3(0, yOffset, 0);
+
+		this.transform.position = this._initialPosition.add(offset);
+	}
 }
 
 SceneManager.RegisterClass("RotatingCube", RotatingCube);
