0477ae77b3e139d3b738d8233faab52d2a2ce6ed
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-09-04 09:05:23 +0200

    Added a BouncingPlateform script that changes the velocity of incoming colliders

---
+++ src/frontend/dev/src/typescript/attachedScripts/BouncingPlateform.ts
@@ -0,0 +1,28 @@
+import { Scene } from "@babylonjs/core/scene";
+import { TransformNode } from "@babylonjs/core/Meshes";
+import { SceneManager, ScriptComponent } from "@babylonjs-toolkit/next";
+import { IPhysicsCollisionEvent, Logger, PhysicsEventType, Vector3 } from "@babylonjs/core";
+
+export class BouncingPlateform extends ScriptComponent {
+    constructor(transform: TransformNode, scene: Scene, properties: any = {}, alias: string = "BouncingPlateform") {
+        super(transform, scene, properties, alias);
+    }
+
+	protected	start()
+	{
+		globalThis.HKP.onTriggerCollisionObservable.add(this.onTriggerEnter.bind(this));
+	}
+
+	private onTriggerEnter(collisionEvent : IPhysicsCollisionEvent) : void
+	{
+		if (collisionEvent.collidedAgainst.transformNode !== this.transform
+			|| collisionEvent.type !== PhysicsEventType.TRIGGER_ENTERED)
+			return ;
+		const	velocity : Vector3 = collisionEvent.collider.getLinearVelocity();
+
+		velocity.scaleInPlace(-1);
+		collisionEvent.collider.setLinearVelocity(velocity);
+	}
+}
+
+SceneManager.RegisterClass("BouncingPlateform", BouncingPlateform);
