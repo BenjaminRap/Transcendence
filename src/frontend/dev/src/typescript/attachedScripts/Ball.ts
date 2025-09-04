c4b667e51f21f71d662494e9698df1b39ceb4f6f
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-09-04 09:03:17 +0200

    The Ball is now using physics instead of manually setting the position

--- src/frontend/dev/src/typescript/attachedScripts/Ball.ts
+++ src/frontend/dev/src/typescript/attachedScripts/Ball.ts
@@ -2,38 +2,31 @@ import { Scene } from "@babylonjs/core/scene";
 import { TransformNode } from "@babylonjs/core/Meshes";
 import { SceneManager, ScriptComponent } from "@babylonjs-toolkit/next";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
-import { PhysicsRaycastResult } from "@babylonjs/core/Physics/physicsRaycastResult";
-import { IPhysicsEngine } from "@babylonjs/core/Physics/IPhysicsEngine";
-import { Logger } from "@babylonjs/core/Misc/logger";
+import { PhysicsBody } from "@babylonjs/core/Physics/v2/physicsBody";
 
 export class Ball extends ScriptComponent {
 	public initialDirection : Vector3 = Vector3.Right();
 	public speed : number = 1;
 
-	private _physicsEngine : IPhysicsEngine;
+	private _physicsBody : PhysicsBody;
 
     constructor(transform: TransformNode, scene: Scene, properties: any = {}, alias: string = "Ball") {
         super(transform, scene, properties, alias);
 		this.initialDirection.normalize();
-
-		const	physicsEngine : IPhysicsEngine | null = this.scene.getPhysicsEngine();
-
-		if (physicsEngine === null)
-			throw new Error("Physics is not enabled in the scene !");
-		this._physicsEngine = physicsEngine;
     }
-	
-	update() : void
-	{
-		const	directionLength : number = this.speed * this.getDeltaTime();
-		const	movement : Vector3 = this.initialDirection.scale(directionLength);
-		const	destination : Vector3 = this.transform.position.add(movement);
-		const	res : PhysicsRaycastResult = this._physicsEngine.raycast(this.transform.position, destination);
 
-		if (res.hasHit) {
-			Logger.Log("Collision at " + res.hitPointWorld);
-		}
-		this.transform.position = destination;
+	protected start()
+	{
+		const	physicsBody = this.getAbstractMesh().getPhysicsBody();
+
+		if (!physicsBody)
+			throw new Error("The Ball script should be attached to a mesh with a physic body !");
+		this._physicsBody = physicsBody;
+	}
+
+	protected ready() : void
+	{
+		this._physicsBody.setLinearVelocity(this.initialDirection.scale(this.speed));
 	}
 }
 
