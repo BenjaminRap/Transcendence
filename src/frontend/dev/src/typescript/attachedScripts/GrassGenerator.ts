7b38a04800be5cd17aa81103d063e25b4027f2eb
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-09-25 09:10:29 +0200

    The GrassGenerator now places the grass in a trapeze depending on the camera view

--- src/frontend/dev/src/typescript/attachedScripts/GrassGenerator.ts
+++ src/frontend/dev/src/typescript/attachedScripts/GrassGenerator.ts
@@ -3,9 +3,17 @@ import { Mesh, TransformNode } from "@babylonjs/core/Meshes";
 import { IUnityTransform, SceneManager, ScriptComponent } from "@babylonjs-toolkit/next";
 
 import { buildGrassMaterial } from "../shaders/grass";
+import { Camera } from "@babylonjs/core/Cameras/camera";
+import { Matrix, Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { getHorizontalFOV, getRandomCoordinatesInTrapeze } from "../utilities";
 
 export class GrassGenerator extends ScriptComponent {
 	private _grassTransform! : IUnityTransform & { mesh : Mesh};
+	private _cameraTransform! : IUnityTransform & { camera : Camera, transform : TransformNode };
+	private _grassInstanceCount : number = 100;
+	private _grassMaxDistance : number = 5;
+
+	private static readonly _horizontalFOVMarginInRadian : number = 0.2;
 
     constructor(transform: TransformNode, scene: Scene, properties: any = {}, alias: string = "GrassGenerator") {
         super(transform, scene, properties, alias);
@@ -13,12 +21,23 @@ export class GrassGenerator extends ScriptComponent {
 
 	protected	awake()
 	{	
+		this.initAttributes();
+		this.setGrassMaterial();
+		this.placeGrassInFrontOfCamera();
+	}
+
+	private	initAttributes()
+	{
 		const	grassMesh = SceneManager.GetTransformNodeByID(this.scene, this._grassTransform.id);
 
 		if (!(grassMesh instanceof Mesh))
 			throw new Error("the _grassMesh transform is not a mesh !!");
 		this._grassTransform.mesh = grassMesh;
-		this.setGrassMaterial();
+
+		this._cameraTransform.transform = SceneManager.GetTransformNodeByID(this.scene, this._cameraTransform.id);
+		this._cameraTransform.camera = SceneManager.FindSceneCameraRig(this._cameraTransform.transform);
+		if (this._cameraTransform.camera === null)
+			throw new Error("the _cameraTransform transform is not a Camera !!");
 	}
 
 	private	setGrassMaterial()
@@ -27,7 +46,28 @@ export class GrassGenerator extends ScriptComponent {
 
 		this._grassTransform.mesh.material = material;
 	}
-}
 
+	private	placeGrassInFrontOfCamera()
+	{
+		const	camera = this._cameraTransform.camera;
+		const	aspectRatio = this.scene.getEngine().getAspectRatio(camera);
+		const	horizontalFOV = getHorizontalFOV(camera.fov, aspectRatio) + GrassGenerator._horizontalFOVMarginInRadian;
+		const	baseForCircleUnit = Math.tan(horizontalFOV / 2) * 2;
+		const	baseNear = baseForCircleUnit * camera.minZ;
+		const	baseFar = baseForCircleUnit * this._grassMaxDistance;
+		const	height = this._grassMaxDistance - camera.minZ;
+
+		for (let index = 0; index < this._grassInstanceCount; index++) {
+			const	localPos2D = getRandomCoordinatesInTrapeze(baseNear, baseFar, height);
+			const	localPos3D = new Vector3(localPos2D.x, -5, localPos2D.y + camera.minZ);
+
+			localPos3D.subtractInPlace(this._grassTransform.mesh.absolutePosition);
+			const	globalPos3D = Vector3.TransformCoordinates(localPos3D, camera.getWorldMatrix());
+
+			const	matrix = Matrix.Translation(globalPos3D.x, globalPos3D.y, globalPos3D.z);
+			this._grassTransform.mesh.thinInstanceAdd(matrix, true);
+		}
+	}
+}
 
 SceneManager.RegisterClass("GrassGenerator", GrassGenerator);
--- src/frontend/dev/src/typescript/utilities.ts
+++ src/frontend/dev/src/typescript/utilities.ts
@@ -1,3 +1,4 @@
+import { Vector2 } from "@babylonjs/core/Maths/math.vector";
 import { Range } from "./Range";
 
 export function randomFromRange(range : Range) : number
@@ -5,3 +6,28 @@ export function randomFromRange(range : Range) : number
   return Math.random() * (range.max - range.min) + range.min;
 }
 
+
+const	square = (value : number) => Math.pow(value, 2);
+const	sqrt = Math.sqrt;
+
+export function	getRandomCoordinatesInTrapeze(baseNear : number, baseFar : number, height : number) : Vector2
+{
+	const	random = Math.random();
+	const	yNumerator = -baseNear + sqrt(square(baseNear) + random * height * (square(baseFar) - square(baseNear)));
+	const	yDenominator = (baseFar - baseNear) / height;
+	const	y : number = yNumerator / yDenominator;
+	const	widhAtZ = remap(y, 0, height, baseNear, baseFar);
+	const	x : number = (Math.random() - 0.5) * widhAtZ;
+
+	return new Vector2(x, y);
+}
+
+function	remap(value : number, min : number, max : number, newMin : number, newMax : number)
+{
+	return newMin + (newMax - newMin) * (value - min) / (max - min);
+}
+
+export function	getHorizontalFOV(verticalFOV : number, aspectRatio : number)
+{
+	return 2 * Math.atan(Math.tan(verticalFOV / 2) * aspectRatio);
+}
