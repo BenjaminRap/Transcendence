eab451cb0ad76aa136b0d8d2cd3b7614a36e2770
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-10-06 15:22:02 +0200

    The height is now flattened around the player

--- src/frontend/dev/src/typescript/attachedScripts/RandomTerrainGenerator.ts
+++ src/frontend/dev/src/typescript/attachedScripts/RandomTerrainGenerator.ts
@@ -1,7 +1,7 @@
 import { Scene } from "@babylonjs/core/scene";
 import { GroundMesh, MeshBuilder, TransformNode } from "@babylonjs/core/Meshes";
 import { IUnityMaterial, IUnityVector2, SceneManager, ScriptComponent } from "@babylonjs-toolkit/next";
-import { int, Vector2, VertexBuffer } from "@babylonjs/core";
+import { float, int, Scalar, Vector2, VertexBuffer } from "@babylonjs/core";
 import { getDiamondSquareArray } from "../diamondSquareAlgorithm";
 import { Range } from "../Range";
 
@@ -9,11 +9,13 @@ import { Float32Array2D } from "../Float32Array2D";
 import { RandomEnvironmentGenerator } from "./RandomEnvironmentGenerator";
 
 export class RandomTerrainGenerator extends ScriptComponent {
-	private _dimensions : IUnityVector2 = new Vector2(100, 100);
+	private _dimension : number = 100;
 	private _subdivisionsFactor : int = 8;
 	private _heightRange : Range = new Range(1, 16);
 	private _randomnessRange : Range = new Range(-2, 2);
 	private _groundMaterial! : IUnityMaterial;
+	private _flattenHeightDistance : number = 20;
+	private _flattenHeightTransitionDistance : number = 100;
 
 	private _ground! : GroundMesh;
 
@@ -44,8 +46,8 @@ export class RandomTerrainGenerator extends ScriptComponent {
 		const	subdivisions = 2 * this._subdivisionsFactor;
 		const	ground = MeshBuilder.CreateGround("ground", {
 			subdivisions: subdivisions,
-			width: this._dimensions.y,
-			height: this._dimensions.x,
+			width: this._dimension,
+			height: this._dimension,
 			updatable: true
 		}, this.scene);
 
@@ -68,11 +70,57 @@ export class RandomTerrainGenerator extends ScriptComponent {
 
 		if (!positions)
 			throw new Error("Can not get the ground vertices data !");
+		const	heightAverage = this.getAverageHeightInCircle(heights);
 		heights.forEach((value : number, _x : int, _y : int, index : number) => {
-			positions[index * 3 + 1] = value;
+			const xWorld = positions[index * 3];
+			const zWorld = positions[index * 3 + 2]; 
+			const flattenedHeight = this.flattenHeight(value, xWorld, zWorld, heightAverage)
+
+			positions[index * 3 + 1] = flattenedHeight;
 		});
 		this._ground.setVerticesData(VertexBuffer.PositionKind, positions, true);
 		this._ground.updateCoordinateHeights();
+	}
+
+	private getAverageHeightInCircle(heights : Float32Array2D) : float
+	{
+		const centerX = heights.width / 2;
+		const centerY = heights.height / 2;
+		const radius = Math.round(this._flattenHeightTransitionDistance * 2 * this._subdivisionsFactor / this._dimension);
+		let sum : float = 0;
+		let summedCount : int = 0;
+
+		for (let y = -radius; y < radius + 1; y++)
+		{
+			const halfWidth : float = Math.sqrt(Math.pow(radius + 0.5, 2) - Math.pow(y, 2));
+			const roundedHalfWidth : int = Math.round(halfWidth);
+
+			for (let x = -roundedHalfWidth; x < roundedHalfWidth + 1; x++)
+			{
+				sum += heights.get(centerX + x, centerY + y);
+				summedCount++;
+			}
+		}
+		if (summedCount == 0)
+			return 0;
+		const average : float = sum / summedCount;
+
+		return average;
+	}
+
+	private	flattenHeight(value : number, x : int, z : int, heightInMiddle : number) : number
+	{
+		const	min  = this._flattenHeightDistance;
+		const	max = this._flattenHeightTransitionDistance;
+		const	distance = Math.sqrt(Math.pow(x, 2) + Math.pow(z, 2));
+		if (distance < min)
+			return heightInMiddle;
+		if (distance > max)
+			return value;
+		const distancePercent = (distance - min) / (max - min);
+		const newValue = Scalar.Lerp(heightInMiddle, value, distancePercent);
+
+		return newValue;
 	}
 
 	private setMaterial()
@@ -84,8 +132,8 @@ export class RandomTerrainGenerator extends ScriptComponent {
 	{
 		if (this._ground === undefined)
 			return 0;
-		if (Math.abs(x) > this._dimensions.x / 2
-			|| Math.abs(y) > this._dimensions.y / 2)
+		if (Math.abs(x) > this._dimension / 2
+			|| Math.abs(y) > this._dimension / 2)
 		{
 			return 0;
 		}
