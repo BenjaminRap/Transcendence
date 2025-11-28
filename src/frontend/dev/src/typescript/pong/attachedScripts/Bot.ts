9157eb0c066cfef4e37bb4711627e2571acab468
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-11-28 09:47:05 +0100

    The bot now follow the ball y position (it beats me already)

--- src/frontend/dev/src/typescript/pong/attachedScripts/Bot.ts
+++ src/frontend/dev/src/typescript/pong/attachedScripts/Bot.ts
@@ -2,8 +2,18 @@ import { Scene } from "@babylonjs/core/scene";
 import { TransformNode } from "@babylonjs/core/Meshes";
 import { SceneManager, ScriptComponent } from "@babylonjs-toolkit/next";
 import { getFrontendSceneData } from "../PongGame";
+import { InputManager, PlayerInput } from "@shared/attachedScripts/InputManager";
+import { Epsilon } from "@babylonjs/core";
 
 export class Bot extends ScriptComponent {
+	private static readonly _paddleMinimumMovement = 0.5;
+	private	_inputManager! : TransformNode;
+	private _paddleRight! : TransformNode;
+	private _paddleLeft! : TransformNode;
+	private _ball! : TransformNode;
+
+	private _inputs! : PlayerInput;
+
     constructor(transform: TransformNode, scene: Scene, properties: any = {}, alias: string = "Bot") {
         super(transform, scene, properties, alias);
 
@@ -13,9 +23,46 @@ export class Bot extends ScriptComponent {
 			SceneManager.DestroyScriptComponent(this);
     }
 
-	protected	awake()
+	protected	start()
 	{
-		console.log("bot !");
+		const	inputManager = SceneManager.GetComponent<InputManager>(this._inputManager, "InputManager", false);
+
+		this._inputs = inputManager.getPlayerInput(1);
+	}
+
+	protected	update()
+	{
+		const	direction = this.getPaddleDirection();
+
+		this.setInput(direction);
+	}
+
+	private	getPaddleDirection() : number
+	{
+		const	direction = this._ball.position.y - this._paddleRight.position.y;
+
+		if (Math.abs(direction) < Bot._paddleMinimumMovement)
+			return 0;
+		return direction;
+	}
+
+	private	setInput(direction : number)
+	{
+		if (direction < 0)
+		{
+			this._inputs.up.setKeyUp();
+			this._inputs.down.setKeyDown();
+		}
+		else if (direction > 0)
+		{
+			this._inputs.up.setKeyDown();
+			this._inputs.down.setKeyUp();
+		}
+		else
+		{
+			this._inputs.up.setKeyUp();
+			this._inputs.down.setKeyUp();
+		}
 	}
 }
 
