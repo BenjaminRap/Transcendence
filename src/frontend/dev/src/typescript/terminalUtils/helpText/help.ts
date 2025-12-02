7c493ee6f58289139e4072b9225bfaa71a9422c9
Isxfiane <sofianebenrhouma93100@gmail.com>
2025-12-02 16:53:25 +0100

    je suis une pute j'ai revert mon commit

---
+++ src/frontend/dev/src/typescript/terminalUtils/helpText/help.ts
@@ -0,0 +1,28 @@
+export let HELP_MESSAGE_NOT_LOG = `
+Help - Utilisation du terminal
+
+Comment exécuter une commande :
+- Tapez une commande, puis appuyez sur Entrée.
+- Le terminal enverra une réponse; chaque réponse commence par le caractère ">".
+
+Statut actuel :
+- Vous êtes actuellement connecté en tant que "guest".
+
+Créer un compte (register) :
+- Tapez: register
+- Appuyez sur Entrée et suivez les instructions affichées pour choisir un nom et un mot de passe.
+
+Se connecter (login) :
+- Tapez login
+- Appuyez sur Entrée et suivez les instructions pour fournir vos identifiants..
+
+Commandes utiles :
+- help : affiche cette aide
+- clear: vide l'écran
+- login / register: se connecter ou créer un compte
+`
+
+
+export let HELP_MESSAGE = `
+Log
+`
