cd84549fdd824e4a1155085ef8642ba1f81d725a
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-12-06 21:16:36 +0100

    Added the content of the OpponentHTMLElement, very basic

---
+++ src/frontend/dev/src/typescript/index.ts
@@ -0,0 +1,1 @@
+import { OpponentGUI } from "./pong/OpponentHTMLElement";
--- src/frontend/dev/src/typescript/pong/OpponentHTMLElement.ts
+++ src/frontend/dev/src/typescript/pong/OpponentHTMLElement.ts
@@ -2,16 +2,25 @@ import { applyTheme, type ThemeName } from "./menuStyles";
 
 export class	OpponentGUI extends HTMLElement
 {
-	constructor(style? : ThemeName)
+	private static readonly _unkownImageUrl : string = "images/unkown.png";
+
+	private	_image : string;
+	private	_name : string;
+
+	constructor(style? : ThemeName, name? : string, image? : string)
 	{
 		super();
+		this._name = name ?? "unkown";
+		this._image = image ?? OpponentGUI._unkownImageUrl;
 		applyTheme(this, style ?? "basic");
 	}
 
 	connectedCallback()
 	{
-		this.classList.add("absolute", "inset-0", "size-full", "z-10", "cursor-default", "select-none", "pointer-events-none");
+		this.classList.add("block", "border-solid", "border-black", "border-[0.2vw]", "rounded-3xl");
 		this.innerHTML = `
+			<img src=${this._image} class="m-auto mt-[5%] w-4/5 h-auto"></img>
+			<p class="mt-[10%] mb-[5%] text-center text-[3vw]">${this._name}</p>
 		`;
 	}
 }
