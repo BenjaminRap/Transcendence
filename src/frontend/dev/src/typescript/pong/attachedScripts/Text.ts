8746e8e405d7a4606db0a178c1b9b54b2ff2800e
Benjamin RAPPO <brappo@desktop-debian-home.home>
2025-12-10 11:32:09 +0100

    Added more imported vraibales to the Text ScriptCOmponent
    Added _maxRow, _fontSizeInPixels, _maxCharactersInRow and _alignment

--- src/frontend/dev/src/typescript/pong/attachedScripts/Text.ts
+++ src/frontend/dev/src/typescript/pong/attachedScripts/Text.ts
@@ -1,15 +1,23 @@
 import { Scene } from "@babylonjs/core/scene";
 import { TransformNode } from "@babylonjs/core/Meshes";
 import { SceneManager } from "@babylonjs-toolkit/next";
-import { AdvancedDynamicTexture, TextBlock } from "@babylonjs/gui/2D"
+import { AdvancedDynamicTexture, Control, TextBlock } from "@babylonjs/gui/2D"
 import { Imported } from "@shared/ImportedDecorator";
 import { Color4 } from "@babylonjs/core";
-import { zodString } from "@shared/ImportedHelpers";
+import { zodInt, zodString } from "@shared/ImportedHelpers";
 import { CustomScriptComponent } from "@shared/CustomScriptComponent";
+import zod from "zod";
+
+const	zodAlignment = zod.literal(["left", "middle", "right"]);
+type	Alignment = zod.infer<typeof zodAlignment>;
 
 export class Text extends CustomScriptComponent {
 	@Imported(zodString) private _text! : string;
 	@Imported(Color4) private _color! : Color4;
+	@Imported(zodInt) private _fontSizeInPixels! : number;
+	@Imported(zodInt) private _maxCharacterInRow! : number;
+	@Imported(zodInt) private _maxRow! : number;
+	@Imported(zodAlignment) private _alignment! : Alignment;
 
 	private _texture! : AdvancedDynamicTexture;
 	private _textBlock! : TextBlock;
@@ -20,12 +28,28 @@ export class Text extends CustomScriptComponent {
 
 	protected awake()
 	{
-		this._texture = AdvancedDynamicTexture.CreateForMesh(this.getAbstractMesh(), 256, 64, false, false, false);
+		const	width = this._fontSizeInPixels * this._maxCharacterInRow;
+		const	height = this._fontSizeInPixels * this._maxRow;
+
+		this._texture = AdvancedDynamicTexture.CreateForMesh(this.getAbstractMesh(), width, height, false, false, false);
 		this._textBlock = new TextBlock(undefined, this._text);
-		this._textBlock.fontSizeInPixels = 64;
+		this._textBlock.fontSizeInPixels = this._fontSizeInPixels;
 		this._textBlock.color = this.colorToString(this._color);
 		this._textBlock.resizeToFit = true;
+		this._textBlock.horizontalAlignment = this.getAlignment();
 		this._texture.addControl(this._textBlock);
+	}
+
+	private getAlignment() : number
+	{
+		switch (this._alignment) {
+			case "left":
+				return Control.HORIZONTAL_ALIGNMENT_LEFT;
+			case "middle":
+				return Control.HORIZONTAL_ALIGNMENT_CENTER;
+			default:
+				return Control.HORIZONTAL_ALIGNMENT_RIGHT;
+		}
 	}
 
 	public setText(content : string)
