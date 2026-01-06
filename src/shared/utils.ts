06a38af14c92a3494a8fc8cfb71538632a3a7753
Benjamin RAPPO <brappo@desktop-debian-home.home>
2026-01-06 22:58:58 +0100

    Added the Result type, a type for returns than can fail

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -14,3 +14,19 @@ export function shuffle<T>(array: T[]) {
         array[randomIndex], array[currentIndex]];
     }
 };
+
+export type Result<ValueType, ErrorType = string> = {
+	success: true,
+	value: ValueType
+} | {
+	success: false,
+	error: ErrorType
+}
+
+export function success<ValueType, ErrorType = string>(value: ValueType): Result<ValueType, ErrorType> {
+    return { success: true, value };
+}
+
+export function error<ValueType, ErrorType = string>(error: ErrorType): Result<ValueType, ErrorType> {
+    return { success: false, error };
+}
