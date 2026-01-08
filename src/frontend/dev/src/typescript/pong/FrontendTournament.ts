041b4d9511c01ff2e96e57a84c6f5fd4f99197c3
Benjamin RAPPO <brappo@desktop-debian-home.home>
2026-01-08 11:10:57 +0100

    Added the FrontendTournament class, it will manage the OnlineTournament at the frontend side
    For now its constructor is private, and there is two static functions to
    create an object : one for joigning and one for creating the tournament.

--- src/frontend/dev/src/typescript/pong/FrontendTournament.ts
+++ src/frontend/dev/src/typescript/pong/FrontendTournament.ts
@@ -1,8 +1,43 @@
+import type { TournamentCreationSettings, TournamentId } from "@shared/ServerMessage";
 import type { ServerProxy } from "./ServerProxy";
+import type { Profile } from "@shared/Profile";
+import { error, success, type Result } from "@shared/utils";
 
 export class	FrontendTournament
 {
-	constructor(serverProxy : ServerProxy)
+	private constructor(
+		private _serverProxy : ServerProxy,
+		private _isCreator : boolean,
+		private _tournamentId : TournamentId,
+		private _participants : Profile[])
 	{
+	}
+
+	public static async createTournament(serverProxy : ServerProxy, settings : TournamentCreationSettings) : Promise<Result<FrontendTournament>>
+	{
+		try {
+			const	tournamentId = await serverProxy.createTournament(settings);
+			const	tournament = new FrontendTournament(serverProxy, true, tournamentId, []);
+
+			return success(tournament);
+		} catch (e) {
+			if (typeof e === "string")
+				return error(e);
+			throw e;
+		}
+	}
+
+	public static async joinTournament(serverProxy : ServerProxy, tournamentId : string) : Promise<Result<FrontendTournament>>
+	{
+		try {
+			const	participants = await serverProxy.joinTournament(tournamentId);
+			const	tournament = new FrontendTournament(serverProxy, false, tournamentId, participants);
+
+			return success(tournament);
+		} catch (e) {
+			if (typeof e === "string")
+				return error(e);
+			throw e;
+		}
 	}
 }
--- src/shared/MessageType.ts
+++ src/shared/MessageType.ts
@@ -1,16 +1,22 @@
-import type { GameInfos, GameInit, KeysUpdate, TournamentCreationSettings, TournamentDescription } from "./ServerMessage";
+import type { Profile } from "./Profile";
+import type { GameInfos, GameInit, KeysUpdate, TournamentCreationSettings, TournamentDescription, TournamentId } from "./ServerMessage";
+import type { Result } from "./utils";
 
-export type ClientMessage = "join-matchmaking" | "ready" | "input-infos" | "forfeit" | "leave-matchmaking" | "create-tournament" | "start-tournament" | "cancel-tournament" | "get-tournaments";
+export type ClientMessage = "join-matchmaking" | "ready" | "input-infos" | "forfeit" | "leave-matchmaking" | "create-tournament" | "start-tournament" | "join-tournament" | "leave-tournament" | "cancel-tournament" | "get-tournaments";
 export type ClientMessageData<T extends ClientMessage> =
 	T extends "input-infos" ? [KeysUpdate] :
-	T extends "create-tournament" ? [TournamentCreationSettings, (error? : string) => void] :
+	T extends "create-tournament" ? [TournamentCreationSettings, (tournamentId : Result<TournamentId>) => void] :
+	T extends "start-tournament" ? [(result : Result<null>) => void] :
+	T extends "join-tournament" ? [TournamentId, (participants : Result<Profile[]>) => void] :
 	T extends "get-tournaments" ? [(descriptions : TournamentDescription[]) => void] :
 	[];
 
-export type ServerEvents = "game-infos" | "joined-game" | "ready" | "forfeit" | "room-closed";
+export type ServerEvents = "game-infos" | "joined-game" | "ready" | "forfeit" | "room-closed" | "tournament-canceled" | "add-participant" | "remove-participant";
 export type ServerEventsData<T extends ServerEvents> =
 	T extends "game-infos" ? [GameInfos] :
 	T extends "joined-game" ? [GameInit] :
+	T extends "add-participant" ? [Profile] :
+	T extends "remove-participant" ? [Profile] :
 	[]
 
 
