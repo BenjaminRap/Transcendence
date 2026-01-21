c24154c0ad45f7b1100daea3381e3fa99d0d5ac2
Benjamin RAPPO <brappo@desktop-debian-home.home>
2026-01-21 10:44:15 +0100

    Fixed the ClosedGUI, it is now smaller, and closer to the border

--- src/frontend/dev/src/typescript/pong/gui/CloseGUI.ts
+++ src/frontend/dev/src/typescript/pong/gui/CloseGUI.ts
@@ -14,7 +14,7 @@ export class	CloseGUI extends HTMLElement implements IGUI<CloseGUIInputs>
 		super();
 		this.classList.add("absolute", "inset-0", "size-full", "cursor-default", "select-none", "pointer-events-none", "z-20");
 		this.innerHTML = `
-			<button class="aspect-square w-[4%] closeGUIClose bg-(--background-color) bg-(image:--background-color) border-(--border-color) border-(length:--border-width) rounded-[20%] absolute right-[2cqw] top-[2cqw] pointer-events-auto hover:scale-(--hover-scale)">
+			<button class="aspect-square w-[3%] closeGUIClose bg-(--background-color) bg-(image:--background-color) border-(--border-color) border-(length:--border-width) rounded-[20%] absolute right-[0.5cqw] top-[0.5cqw] pointer-events-auto hover:scale-(--hover-scale)">
 				<div class="aspect-square m-auto flex flex-col h-3/5 rotate-45">
 					<div class="w-[13.8%] h-[45%] bg-(--border-color) m-auto"></div>
 					<div class="w-full h-[13.8%] bg-(--border-color)"></div>
