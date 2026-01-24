45f713fd2e86e777a4709bd0d0f196ae689e923c
Benjamin RAPPO <brappo@desktop-debian-home.home>
2026-01-24 12:06:25 +0100

    Fixed the OnGameMessage and onTournamentMessage being called after the ServerProxy ones

--- src/frontend/dev/src/typescript/pong/PongGame.ts
+++ src/frontend/dev/src/typescript/pong/PongGame.ts
@@ -58,8 +58,8 @@ export class PongGame extends HTMLElement {
 		}, this, false);
 
 		this.append(this._canvas);
-		this._serverProxy.onTournamentMessage().add(tournamentEvent => this.onTournamentMessage(tournamentEvent));
-		this._serverProxy.onGameMessage().add(gameEvent => this.onGameMessage(gameEvent));
+		this._serverProxy.onTournamentMessage().add(tournamentEvent => this.onTournamentMessage(tournamentEvent), undefined, true);
+		this._serverProxy.onGameMessage().add(gameEvent => this.onGameMessage(gameEvent), undefined, true);
 		this.loadGame();
 	}
 
