52f14b75083d973c11a5dc6ee70db8c550e712aa
Florian <berthouly.florian@gmail.com>
2026-01-24 16:09:11 +0100

    fix bug avatar

--- src/backend/typescript/services/AuthService.ts
+++ src/backend/typescript/services/AuthService.ts
@@ -33,7 +33,7 @@ export class AuthService {
                 username: data.username,
                 email: data.email,
                 password: hashedPassword,
-                avatar: process.env.DEFAULT_AVATAR_URL,
+                avatar: process.env.DEFAULT_AVATAR_URL || "api/static/public/avatarDefault.webp",
             },
         });
 
