95e70784f8f42c00cf9c0e921ed6800cbe79618f
Benjamin RAPPO <brappo@desktop-debian-home.home>
2026-01-28 09:32:23 +0100

    ReRenamed 2 vs 2 to 1 vs 1

--- src/frontend/dev/src/typescript/pong/gui/GameTypeChoiceGUI.ts
+++ src/frontend/dev/src/typescript/pong/gui/GameTypeChoiceGUI.ts
@@ -16,7 +16,7 @@ export class	GameTypeChoiceGUI extends HTMLElement implements IGUI<GameTypeChoic
 		this.classList.add("absolute", "inset-0", "size-full", "cursor-default", "select-none", "pointer-events-none", "backdrop-blur-sm");
 		this.innerHTML = `
 			<div class="flex flex-col size-full h-4/6 w-1/4 left-1/2 -translate-1/2 top-1/2 absolute">
-				${this.getButtonHTML("2 vs 2", "GameTypeChoiceGUITwoVersusTwo")}
+				${this.getButtonHTML("1 vs 1", "GameTypeChoiceGUITwoVersusTwo")}
 				${this.getButtonHTML("Tournament", "GameTypeChoiceGUITournament")}
 				${this.getButtonHTML("Cancel", "GameTypeChoiceGUICancel")}
 			</div>
